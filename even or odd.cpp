0f78a662631d1427fb6e5ffb0148aea5014e6ece
sigiralvinuthna <113973694+sigiralvinuthna@users.noreply.github.com>
2022-09-22 14:03:45 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+ int Size, i, a[10];
+ int Even_Count = 0, Odd_Count = 0;
+ printf("\n Please Enter the Size of an Array : ");
+ scanf("%d", &Size);
+ printf("\nPlease Enter the Array Elements\n");
+ for(i = 0; i < Size; i++)
+{
+ scanf("%d", &a[i]);
+}
+
+for(i = 0; i < Size; i ++)
+{
+ if(a[i] % 2 == 0)
+ {
+ Even_Count++;
+ }
+ else
+ {
+ Odd_Count++;
+}
+}
+ printf("\n Total Number of Even Numbers in this Array=%d",Even_Count);
+ printf("\ntotal number of odd number in this array=%d",Odd_Count);
+ return 0;
+}
+
