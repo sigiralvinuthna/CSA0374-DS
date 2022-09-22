379394c62cf1a5c22f5afff6c16e7327ed3ee096
sigiralvinuthna <113973694+sigiralvinuthna@users.noreply.github.com>
2022-09-22 14:45:50 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],search,c,n;
+	printf("enter the sixe of a array");
+	scanf("%d",&n);
+	printf("enter %d integer\n",n);
+	for(c=0;c<n;c++)
+	printf("enter a number to search");
+	scanf("%d",&search);
+	for(c=0;c<n;c++)
+	{
+		if(a[c]==search)
+		{
+			printf("%d is present at location %d \n",search,c+1);
+			break;
+		}
+	}
+	if(c==n)
+	{
+		printf("%D is not present in array\n",search);
+	}
+	return 0;
+}
